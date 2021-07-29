603d7597d8635b81c1d621bd915c8a5916e427c1
Harsh Tripathi <ht87374@gmail.com>
2021-07-29 20:09:07 +0530

    changed title name

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Smart Home Appliances',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Ubuntu',
