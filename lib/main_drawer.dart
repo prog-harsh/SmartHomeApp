6f8a4c47643fee7270762054fedee79f59e2f5d1
Harsh Tripathi <ht87374@gmail.com>
2021-07-29 20:10:29 +0530

    added image and contact us screen

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:myhome/screens/contact_us_screen.dart';
 
 class MainDrawer extends StatelessWidget {
+  MainDrawer(this._image, this._userName);
+  final _image;
+  final _userName;
   final _auth = FirebaseAuth.instance;
 
   @override
@@ -10,30 +14,68 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           Container(
+            margin: const EdgeInsets.only(top: 25),
             height: 150,
-            color: Colors.blue,
+            width: double.infinity,
+            //color: Colors.blue,
+            child: Image(
+              image: _image,
+              fit: BoxFit.contain,
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              onTap: () async {
-                await _auth.signOut();
-                Navigator.of(context).pushReplacementNamed('login-screen');
-              },
-              title: Text(
-                'Sign Out',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                ),
-              ),
-              leading: Icon(
-                Icons.logout,
-                color: Colors.black87,
-                size: 26,
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Welcome, $_userName',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontSize: 20,
               ),
             ),
-          )
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.mail_outline,
+              color: Colors.black87,
+              size: 26,
+            ),
+            title: Text(
+              'Contact Us',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => ContactUsScreen(),
+                ),
+              );
+            },
+            selectedTileColor: Colors.amber,
+          ),
+          ListTile(
+            onTap: () async {
+              await _auth.signOut();
+              Navigator.of(context).pushReplacementNamed('login-screen');
+            },
+            title: Text(
+              'Sign Out',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            ),
+            leading: Icon(
+              Icons.logout,
+              color: Colors.black87,
+              size: 26,
+            ),
+          ),
         ],
       ),
     );
